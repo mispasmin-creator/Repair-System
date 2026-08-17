76941f3eae51b32c7c11d8d4a9b36fa467d457d9
Ahitesh <ahitesh11@github.com>
2026-08-17 11:38:10 +0530

    first commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,7 +6,7 @@
 //   const context = useContext(AuthContext);
 //   if (!context) {
 //     throw new Error('useAuth must be used within an AuthProvider');
-//   }
+//   } 
 //   return context;
 // };
 
